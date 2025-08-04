f164ba4a901bf697093b40f53a808b6532fd5788
Siddhu83 <siddhardhap06@gmail.com>
2025-08-04 22:03:01 +0530

    reduced the unnecessary comprehesion and changed the condition

--- group_agrams.py
+++ group_agrams.py
@@ -4,10 +4,13 @@ def sort_str(my_str):
     return ''.join(sorted(my_str))
 
 def group_anagrams(strs):
-    anagrams_dict = {sort_str(s): [] for s in strs}
+    anagrams_dict = {}
     for i in strs:
         c = sort_str(i)
-        anagrams_dict[c].append(i)
+        if c not in anagrams_dict:
+            anagrams_dict[c] = [i]
+        else:
+            anagrams_dict[c].append(i)
     
     return list(anagrams_dict.values())
 
