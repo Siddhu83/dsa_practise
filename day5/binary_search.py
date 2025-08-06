ec0779f417222526d401ee34ea3655e874cdd33c
Siddhu83 <siddhardhap06@gmail.com>
2025-08-06 21:35:42 +0530

    added the binary_search

---
+++ day5/binary_search.py
@@ -0,0 +1,14 @@
+# this program illustrates the binary search
+
+def search(nums, target):
+        l, r = 0, len(nums) -1
+
+        while l <= r :
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
