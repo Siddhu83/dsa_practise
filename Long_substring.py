02dea353093c4c92238e893dd3df978e4f1a45b3
Siddhu83 <siddhardhap06@gmail.com>
2025-08-07 21:47:34 +0530

    Create Long_substring.py

---
+++ Long_substring.py
@@ -0,0 +1,11 @@
+def length_of_longest_substring(s: str) -> int:
+    last_seen = {}
+    max_len = 0
+    left = 0
+
+    for right, ch in enumerate(s):
+        if ch in last_seen and last_seen[ch] >= left:
+            left = last_seen[ch] + 1
+        last_seen[ch] = right
+        max_len = max(max_len, right - left + 1)
+    return max_len
