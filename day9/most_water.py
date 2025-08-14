bdd49031ff011999b59e13df3a0eef05e83f8d2b
Siddhu83 <siddhardhap06@gmail.com>
2025-08-14 20:07:00 +0530

    Added the solution of the most_water & sub_array_sum

--- day9/most_water.py
+++ day9/most_water.py
@@ -1,1 +1,18 @@
 #leetcode question 112: container with most water
+
+def max_area(height: list[int]) -> int:
+    left, right = 0, len(height) - 1
+    max_area = 0
+    
+    while left < right:
+        width = right - left
+        current_height = min(height[left], height[right])
+        current_area = width * current_height
+        max_area = max(max_area, current_area)
+        
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+            
+    return max_area
--- day9/sub_array_sum.py
+++ day9/sub_array_sum.py
@@ -1,1 +1,21 @@
 #leetcode question 560: subarray sum equals k
+
+def subarray_sum(nums: list[int], k: int) -> int:
+    count = 0
+    cumulative_sum = 0
+    sum_map = {0: 1}  # Initialize with sum 0 having one occurrence
+    
+    for num in nums:
+        cumulative_sum += num
+        
+        # Check if (cumulative_sum - k) exists in the map
+        if cumulative_sum - k in sum_map:
+            count += sum_map[cumulative_sum - k]
+        
+        # Update the map with the current cumulative sum
+        if cumulative_sum in sum_map:
+            sum_map[cumulative_sum] += 1
+        else:
+            sum_map[cumulative_sum] = 1
+            
+    return count
