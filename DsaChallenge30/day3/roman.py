5e4e2fcd03652fef52050b28a74a12a6124dff57
Siddhu83 <siddhardhap06@gmail.com>
2025-08-22 21:13:42 +0530

    added the solution of int to roman

---
+++ DsaChallenge30/day3/roman.py
@@ -0,0 +1,14 @@
+# following function takes the input a number and retrun its roman number form
+
+def intToRoman(num: int) -> str:
+    roman = ''
+    roman_numerals = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
+    for value, numeral in roman_numerals:
+        while num >= value:
+            roman += numeral
+            num -= value
+    return roman
