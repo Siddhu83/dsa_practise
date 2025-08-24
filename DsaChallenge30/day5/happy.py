e19ef77ec952158fc7236f2fbb6048354cdd5345
Siddhu83 <siddhardhap06@gmail.com>
2025-08-24 19:35:13 +0530

    added the solution of the no of 1s in binary form

--- DsaChallenge30/day5/happy.py
+++ DsaChallenge30/day5/happy.py
@@ -1,18 +1,18 @@
 def calcSum(n):
     result = 0
-    while n:
+    while n > 0:
         result += (n % 10) ** 2
         n //= 10
     return result
 
 class Solution:
     def isHappy(self, n: int) -> bool:
-        cycle_set = set()
+        cycle_set = []
         n = calcSum(n)
         while n != 1:
             n = calcSum(n)
             print(n)
             if n in cycle_set:
                 return False
-            cycle_set.add(n)
+            cycle_set.append(n)
         return True
