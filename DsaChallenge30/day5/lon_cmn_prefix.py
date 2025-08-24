30ac78f4f27ac238a627917cda73afb1d4b7f987
Siddhu83 <siddhardhap06@gmail.com>
2025-08-24 19:50:51 +0530

    added the solution of the longest common prefix

---
+++ DsaChallenge30/day5/lon_cmn_prefix.py
@@ -0,0 +1,14 @@
+def commonPrefix(str1, str2):
+    common = ''
+    for i in zip(str1, str2):
+        if i[0] != i[1]:
+            return common
+        common += i[0]
+    return common
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        common = strs[0]
+        for i in range(1, len(strs)):
+            common = commonPrefix(common, strs[i])
+        return common
