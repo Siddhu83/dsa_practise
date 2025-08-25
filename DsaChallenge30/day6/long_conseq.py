8510d91269d2a6bfea0e3150c608cb1e29ed878c
Siddhu83 <siddhardhap06@gmail.com>
2025-08-25 21:34:37 +0530

    added the solution of the longest consequtive sequence

--- DsaChallenge30/day6/long_conseq.py
+++ DsaChallenge30/day6/long_conseq.py
@@ -1,4 +1,20 @@
 # leetcode 128: longest_conseqqutive-sequence
+from typing import List
 
 class Solution:
-    
+    def longest_consecutive(nums: List[int]) -> int:
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:  # start of sequence
+                current = num
+                length = 1
+
+                while current + 1 in num_set:
+                    current += 1
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
