66550672a7d0d8ad5be264d399c7b79a633d012a
Siddhu83 <siddhardhap06@gmail.com>
2025-08-26 20:42:59 +0530

    Added the solution to string compression

---
+++ DsaChallenge30/str_compress.py
@@ -0,0 +1,19 @@
+def compress(chars: List[str]) -> int:
+    read = write = 0
+    n = len(chars)
+
+    while read < n:
+        char = chars[read]
+        count = 0
+        while read < n and chars[read] == char:
+            read += 1
+            count += 1
+
+        chars[write] = char
+        write += 1
+        if count > 1:
+            for digit in str(count):
+                chars[write] = digit
+                write += 1
+
+    return write
