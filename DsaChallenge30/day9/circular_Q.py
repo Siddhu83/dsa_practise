0ac7da90729b6c858033bfff2625ffc714518b3a
Siddhu83 <siddhardhap06@gmail.com>
2025-08-28 21:22:08 +0530

    added the solution to circular queue

---
+++ DsaChallenge30/day9/circular_Q.py
@@ -0,0 +1,40 @@
+# leetcode question: https://leetcode.com/problems/design-circular-queue/
+
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.queue = [0] * k
+        self.head = 0
+        self.tail = 0
+        self.size = 0
+        self.capacity = k
+
+    def enQueue(self, value: int) -> bool:
+        if self.size == self.capacity:
+            return False
+        self.queue[self.tail] = value
+        self.tail = (self.tail + 1) % self.capacity
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.size == 0:
+            return False
+        self.head = (self.head + 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.size == 0:
+            return -1
+        return self.queue[self.head]
+
+    def Rear(self) -> int:
+        if self.size == 0:
+            return -1
+        return self.queue[(self.tail - 1 + self.capacity) % self.capacity]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.capacity
